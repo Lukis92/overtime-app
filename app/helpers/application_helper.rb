88dc3eb207eb5ee4e102301cee9e99d41e4c79d0
Łukasz Korol <lukas.korol@gmail.com>
2017-01-11 21:21:25 +0100

    Added error notifications for post form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def active?(path)
     'active' if current_page?(path)
   end
+
+  def errors_for(object)
+    render 'shared/errors', object: object if object.errors.any?
+  end
 end
