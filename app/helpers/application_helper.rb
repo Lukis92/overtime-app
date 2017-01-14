12f8c3fe669c3daabcd3a62ebf17b08b32f8e72b
Łukasz Korol <lukas.korol@gmail.com>
2017-01-14 10:58:07 +0100

    Integrated application helper for status labels and integrated enums for audit log

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,25 @@ module ApplicationHelper
   def errors_for(object)
     render 'shared/errors', object: object if object.errors.any?
   end
+
+  def status_label(status)
+    status_span_generator status
+  end
+
+  private
+
+  def status_span_generator(status)
+    case status
+    when 'submitted'
+      content_tag(:span, status.titleize, class: 'label label-primary')
+    when 'approved'
+      content_tag(:span, status.titleize, class: 'label label-success')
+    when 'rejected'
+      content_tag(:span, status.titleize, class: 'label label-danger')
+    when 'pending'
+      content_tag(:span, status.titleize, class: 'label label-primary')
+    when 'confirmed'
+      content_tag(:span, status.titleize, class: 'label label-success')
+    end
+  end
 end
