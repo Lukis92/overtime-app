7286086787d847c314a78efef105a6ee5e2f4546
Łukasz Korol <lukas.korol@gmail.com>
2017-01-14 23:59:33 +0100

    Added mailer config requirement to production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,4 +13,5 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
   config.serve_static_files = true
+  config.action_mailer.default_url_options = { host: 'lk-overtime.herokuapp.com' }
 end
