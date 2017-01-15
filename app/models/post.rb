f57c5173dfff776a497ee99ce9319c0f82c5cb92
Łukasz Korol <lukas.korol@gmail.com>
2017-01-15 11:52:11 +0100

    Implemented work performed instead rationale

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,7 +15,7 @@
 class Post < ActiveRecord::Base
   enum status: { submitted: 0, approved: 1, rejected: 2 }
   belongs_to :user
-  validates_presence_of :date, :rationale, :daily_hours
+  validates_presence_of :date, :work_performed, :daily_hours
   validates :daily_hours, numericality: { greater_than: 0.0 }
   scope :posts_by, ->(user) { where(user_id: user.id) }
   after_save :confirm_audit_log, if: :submitted?
